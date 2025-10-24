ad2e9b4ffbbe61a554e2a5b00e79cedc3c9339f5
SDx07 <174770705+SDx07@users.noreply.github.com>
2025-10-24 21:26:12 +0530

    update

--- backend/src/models/userModel.ts
+++ backend/src/models/userModel.ts
@@ -1,5 +1,36 @@
-const mongoose = require("mongoose");
+import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({});
+interface IUserProps extends mongoose.Document {
+  username: string;
+  mobileNumber: string;
+  address: string;
+  status: "active" | "blocked";
+}
 
-module.exports = mongoose.model("UserModel", userSchema);
+const userSchema: mongoose.Schema<IUserProps> = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    mobileNumber: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    address: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    status: {
+      type: String,
+      enum: ["active", "blocked"],
+      required: true,
+    },
+  },
+  { timestamps: true, collection: "users" },
+);
+
+export default mongoose.model<IUserProps>("UserModel", userSchema);
