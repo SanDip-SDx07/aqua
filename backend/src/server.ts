901fee0f3af8994f606a106e4d47c2536dba92e9
SDx07 <174770705+SDx07@users.noreply.github.com>
2025-10-24 22:22:00 +0530

    update

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,8 +1,7 @@
 import express from "express";
 import cors from "cors";
-import rateLimit from "express-rate-limit";
 import cookieParser from "cookie-parser";
-
+import rateLimit from "express-rate-limit";
 import connectMongoDB from "./config/connectMongoDB";
 
 const app = express();
